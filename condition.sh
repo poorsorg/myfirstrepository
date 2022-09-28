88e438f4d9aaafdab995f463be72f7462ecf4135
rahiqfahim <rahiq.ff@gmail.com>
2022-09-28 17:34:54 -0500

    adding a new script to the condition fie

---
+++ condition.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "please enter a number:" number
+number=0
+i=1
+if [[ $numer -gt $i ]]; then
+	echo "your number is a positive value"
+	i=+1
+else
+	echo "your bumber is a negative value"
+fi
