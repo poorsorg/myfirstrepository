185d5b8749326242ce0f55d33c18e9d8341eb843
rahiqfahim <rahiq.ff@gmail.com>
2022-10-09 22:08:41 -0500

    removed mvn version

--- pipeline {.groovy
+++ pipeline {.groovy
@@ -14,12 +14,13 @@ pipeline {
             steps {
                 echo "get up at 8"
                 echo "job time plz wake up otherwise you will be screwed up"
+                echo "go to the job plz"
             }
         }
         stage('now get fresh') {
             steps {
                 echo "take the bike"
-                echo "go to the walmart and pick up the item"
+            echo "go to the walmart and pick up the item"
             }
         }
     
